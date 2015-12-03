924818545ff2c43ea768d00b2a11ccf0db27a29a
Pablite5 <Pablite5@GAMING>
2015-12-03 09:41:42 +0100

    FINAL

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -31,9 +31,7 @@
 
 package com.toy.anagrams.lib;
 
-/**
- * Implementation of the logic for the Anagram Game application.
- */
+
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
